3addb3601d1eafd2f858bc79b9820c2f48066c5a
Leki <lekisintole82@gmail.com>
2024-04-21 22:02:16 +0300

    use of variable

--- project1.php
+++ project1.php
@@ -7,12 +7,13 @@
 </head>
 <body>
     <?php  
-    
+    $paragraph = "paragraph";
+    $number= "number";
     echo"<h1>Emmanuel</h1>";
-    echo"<p>This is a paragraph 1</p>";
-    echo"<p>This is a paragraph 2</p>";
-    echo"<p>This is a paragraph 3</p>";
-    echo"<p>This is a paragraph 4</p>";
+    echo"<p>This is a $paragraph 1</p>";
+    echo"<p>This is a $paragraph 2</p>";
+    echo"<p>This is a $paragraph $number 3</p>";
+    echo"<p>This is a $paragraph $number 4</p>";
     echo "<hr>";
    
     echo"<p>My favorite color is blue.</p"
