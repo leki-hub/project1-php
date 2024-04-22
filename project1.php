ae9766e69c6560c9c0e7809281d345eab2fba7d1
Leki <lekisintole82@gmail.com>
2024-04-22 22:46:11 +0300

    associative array

--- project1.php
+++ project1.php
@@ -7,23 +7,17 @@
 </head>
 <body>
 <form action="project1.php" method= "post">
-APPLE:<input type="checkbox" name="fruits[]" value="apple"><br>
-Mango:<input type="checkbox" name="fruits[]" value="mango"><br>
-Passion:<input type="checkbox" name="fruits[]" value="passion"><br>
-Orange:<input type="checkbox" name="fruits[]" value="orange"><br>
+Student:<input type="textbox" value="manu"><br>
+
 <input type=submit value="Submit">
 
 
 </form>
 <br/>
 <?php
-$friends= array('God', 'Jesus'); 
- echo "My Friends: $friends[0]";    
- $friends[6] = "James";
-echo count( $friends);  
-
-$fruitsArray= $_POST["fruits"];   
-echo $fruitsArray[0];   
+// Asscocative arrays, are like map object, since they have key value pairs.
+$apples = array("JJ"=>"A+","Kim"=>"B-", "Jim"=>"C+");  // Key is the fruit and Value is variety of apple
+echo $apples["JJ"]."<br>";   // Accessing using key
 ?>
 
 </body>
